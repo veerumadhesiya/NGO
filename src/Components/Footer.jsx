5d15e412b805d3b96fdfcc2b20f756e68a3c9e84
Veeru Madhesiya <veerumadheshiya009@gmail.com>
2026-03-28 06:32:47 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white text-center py-6">
-      <p>&copy; {new Date().getFullYear()} My NGO. All rights reserveds.</p>
+      <p>&copy; {new Date().getFullYear()} My NGO. All right reserveds.</p>
     </footer>
   );
 }
